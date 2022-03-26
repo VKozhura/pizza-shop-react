60ae1c41645a69472991f06657a85605cd75284c
VKozhura <kozhura_v@mail.ru>
2022-03-26 11:24:46 +0300

    реализован компонент PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 
 const App = () => {
+	const [pizzas, setPizzas] = React.useState([]);
+
+	useEffect(() => {
+		axios.get("http://localhost:3000/db.json").then((response) => setPizzas(response.data.pizzas));
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header />
 			<div className="content">
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route path="/" element={<Home pizzas={pizzas} />} />
 					<Route path="/cart" element={<Cart />} />
 				</Routes>
 			</div>
